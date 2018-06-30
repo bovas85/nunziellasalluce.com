6c392295565e06551378394bd957adc0bfbf0872
Alessandro Giordo <bovas85@gmail.com>
2018-06-30 22:43:11 +0100

    added sass helpers

--- components/Nav/AppNav.vue
+++ components/Nav/AppNav.vue
@@ -1,109 +1,26 @@
 <template>
 <div class="navigation-bar">
-  <div class="transition-overlay" :class="{'is-active': $store.state.routeTransition}">
-    <div class="preloader"></div>
-    <span class="pulse"></span>
-    <span class="pulse"></span>
-    <span class="pulse"></span>
-  </div>
   <cookie-box></cookie-box>
   <div class="navigation" :class="{'is-padded': getNavPadding, 'blue-bg': checkStyle}">
     <nav role="navigation" class="container-fluid grid-wrapper is-gapless navbar">
-      <span class="menu is-hidden-tablet col--2-mobile col--3-tablet is-left" @click="$store.commit('openMobileMenu')">
-        <div class="menu-icons">
-          <div class="menu-icon"></div>
-          <div class="menu-icon"></div>
-          <div class="menu-icon"></div>
-        </div>
-        <span class="text hidden-mobile">Menu</span>
-      </span>
-      <span class="menu hidden-mobile is-hidden-mobile-large-only col--2-mobile col--4-tablet is-left" @click="$store.commit('openDestinationMenu')">
-        <span class="text destination hidden-mobile">Destinations</span>
-        <i class="fa fa-arrow-down"></i>
-      </span>
-      <nuxt-link to="/" @click="refreshPage()" class="logo col--8-mobile col--4-tablet is-center"></nuxt-link>
-      <div class="search col--2-mobile col--4-tablet is-right">
-        <div class="is-flex" @click="$store.commit('openSearch')">
-          <span class="hidden-mobile">Search</span>
-          <div class="search"></div>
-        </div>
-        <span class="menu menu--desktop hidden-mobile is-hidden-mobile-large-only" @click="$store.commit('openMenu')">
-          <span class="text hidden-mobile">Menu</span>
-          <div class="menu-icons">
-            <div class="menu-icon"></div>
-            <div class="menu-icon"></div>
-            <div class="menu-icon"></div>
-          </div>
-        </span>
-      </div>
+
     </nav>
   </div>
-
-
-  <no-ssr>
-    <vue-media :query="{maxWidth: 767}">
-      <div style="z-index: 9999" class="modal-container is-hidden-tablet">
-        <app-menu-mobile></app-menu-mobile>
-      </div>
-    </vue-media>
-  </no-ssr>
-
-  <no-ssr>
-    <vue-media :query="{minWidth: 768}">
-      <div style="z-index: 9999" class="modal-container hidden-mobile is-hidden-mobile-large-only">
-        <destination-menu></destination-menu>
-      </div>
-    </vue-media>
-  </no-ssr>
-
-
-  <app-search></app-search>
-
-  <no-ssr>
-    <vue-media :query="{minWidth: 767}">
-      <div
-        style="z-index: 9999"
-        class="modal-container modal-container-large app-menu hidden-mobile is-hidden-mobile-large-only"
-      >
-        <app-menu></app-menu>
-      </div>
-    </vue-media>
-  </no-ssr>
 
 </div>
 
 </template>
 
 <script>
-  import Config from "~/assets/config";
   import debounce from "lodash/debounce";
 
   export default {
     name: "AppNav",
-    props: {
-      journal: {
-        type: Boolean,
-        default: false
-      }
-    },
     components: {
-      AppMenuMobile: () => import("@/components/Nav/AppMenuMobile"),
-      AppMenu: () => import("@/components/Nav/AppMenu"),
-      AppSearch: () => import("@/components/Nav/AppSearch"),
-      DestinationMenu: () => import("@/components/Nav/DestinationMenu"),
       CookieBox: () => import("@/components/UI/CookieBox")
     },
     data () {
       return {
-        focus: false,
-        results: [],
-        searchInput: "",
-        disabled: "",
-        openMenuTitle: "",
-        submenuSelectionMade: false,
-        menuLeft: false,
-        latest: {},
-        menuData: {}
       };
     },
     methods: {
@@ -115,80 +32,10 @@
       }
     },
     watch: {
-      // sorcery to check for updates on modal state boolean
-      // if true, it will run some DOM stuff to block scrolling else remove it
-      modalState () {
-        if (this.modalState) {
-          let body = document.querySelector("body");
-          let html = document.querySelector("html");
-          if (body && html) {
-            setTimeout(() => {
-              body.style.overflow = "hidden";
-              html.style.overflow = "hidden";
-            }, 600);
-
-            if (window.innerWidth < 577) {
-              // body.style.position = 'fixed'
-            }
-          }
-        } else {
-          let body = document.querySelector("body");
-          let html = document.querySelector("html");
-          if (body && html) {
-            setTimeout(() => {
-              body.style.overflow = "visible";
-              html.style.overflow = "visible";
-              // body.style.position = 'static'
-            }, 600);
-          }
-        }
-      }
     },
     computed: {
-      menuBg () {
-        if (this.currentMenu) {
-          if (this.currentMenu === "kids") {
-            return {
-              "background-color": "#e1f2f2"
-            };
-          } else if (this.currentMenu === "offers") {
-            return {
-              "background-color": "#fcf2f0"
-            };
-          } else if (this.currentMenu === "events") {
-            return {
-              "background-color": "#dceaf7"
-            };
-          } else {
-            return {
-              "background-color": "#e1f2f2"
-            };
-          }
-        }
-      },
-      modalState () {
-        // get modal state from the store and compute it
-        return this.$store.state.modalOpen;
-      },
-      getNavPadding () {
-        return this.$store.state.navPadding;
-      },
-      checkStyle () {
-        if (this.journal) return true;
-        var found = 0;
-        if (this.$store.state.continents != null) {
-          this.$store.state.continents.forEach(continent => {
-            // console.log(continent.slug, this.$route.name)
-            if (continent.slug === this.$route.name) {
-              found++;
-            }
-          });
-          return found > 0;
-        } else return false;
-      }
     },
     created () {
-      this.$store.commit("routeTransition");
     },
     async mounted () {
       if (process.browser) {
@@ -223,13 +70,9 @@
             }, 300)
           );
         });
-        this.$store.commit("closeSearch");
 
         // route management for menus/state/transitions
         this.$root.$on("routeChanged", () => {
-          // console.log('route changed, transitioning')
-          this.$store.commit("routeTransition");
-          this.$store.commit("resetMenus");
           let body = document.querySelector("body");
           let html = document.querySelector("html");
           if (body && html) {
@@ -244,75 +87,6 @@
 </script>
 
 <style lang="scss" scoped>
-  .transition-overlay {
-    background: #fafafa;
-    position: fixed;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    opacity: 0;
-    z-index: -1;
-    will-change: transform, z-index;
-    transform: translateZ(0);
-    backface-visibility: hidden;
-    transition: opacity 0.3s ease-out, z-index 0.2s ease-in-out;
-    &.is-active {
-      z-index: 999999;
-      opacity: 1;
-    }
-    .preloader {
-      width: 50px;
-      height: 64px;
-      background-image: url("/images/loader-logo.svg");
-      background-repeat: no-repeat;
-      background-size: contain;
-    }
-    .pulse {
-      z-index: 9999999990;
-      width: 50px;
-      height: 64px;
-      position: absolute;
-      border-radius: 50%;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      &:nth-of-type(2):before {
-        animation-delay: 0.6s;
-      }
-      &:nth-of-type(3):before {
-        animation-delay: 2.2s;
-      }
-      &:before {
-        content: "";
-        display: block;
-        position: relative;
-        width: 50px;
-        height: 64px;
-        border-radius: 50%;
-        border-style: solid;
-        border-width: thin;
-        border-color: #000b19;
-        animation: scale-before 3.5s infinite ease;
-      }
-    }
-    @keyframes scale-before {
-      0% {
-        transform: scale(1);
-        opacity: 0;
-      }
-      40% {
-        opacity: 0.5;
-      }
-      100% {
-        transform: scale(1.4);
-        opacity: 0;
-      }
-    }
-  }
   .navigation {
     position: absolute;
     top: 0;
