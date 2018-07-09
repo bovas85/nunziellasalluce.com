4faa3f77045c02296c74d7dc14dc37d7f1d75848
Alessandro <alex.g@nakedideas.com>
2018-07-09 13:25:30 +0100

    fixed issue on style for testimonial image

--- components/UI/TheTestimonial.vue
+++ components/UI/TheTestimonial.vue
@@ -11,45 +11,50 @@
 </template>
 
 <script>
-    export default {
-      name: 'TheTestimonial',
-      props: {
-        data: {
-          type: Object
-        }
-      },
-      computed: {
-        testimonial () {
-          if (!this.data || this.data == null) return false
-          return this.data.acf
-        }
+  export default {
+    name: 'TheTestimonial',
+    props: {
+      data: {
+        type: Object
+      }
+    },
+    computed: {
+      testimonial () {
+        if (!this.data || this.data == null) return false
+        return this.data.acf
       }
     }
+  }
 </script>
 
 <style lang="scss" scoped>
-    .testimonial {
-      display: flex;
-      flex-direction: column;
-      flex-basis: calc(33.3333% - 16px);
-      padding: $gap 0;
+  .testimonial {
+    display: flex;
+    flex-direction: column;
+    flex-basis: calc(33.3333% - 16px);
+    padding: $gap 0;
 
-      img {
-        border-radius: 100%;
-        object-fit: contain;
-      }
+    img {
+      border-radius: 100%;
+      width: 60px;
+      height: 60px;
+      object-fit: contain;
+      margin: 0 auto;
+      margin-bottom: $gap / 2;
+    }
 
-      blockquote {
-        font-style: italic;
-        font-weight: 400;
-        padding-top: $gap / 2;
+    blockquote {
+      font-style: italic;
+      font-weight: 400;
+      border-left: 3px solid #ccc;
+      padding-left: $gap / 2;
 
-        span {
-          padding-top: $gap / 3;
-          display: block;
-          font-style: normal;
-          font-weight: 600;
-        }
+      span {
+        padding-top: $gap / 3;
+        display: block;
+        font-style: normal;
+        font-weight: 600;
       }
     }
+  }
 </style>
