1f47cc73819277f30d8f0f437bc18ed290be36fd
Alessandro Giordo <16958684+bovas85@users.noreply.github.com>
2018-08-12 13:35:13 +0100

    fixed prev next

--- pages/_work/index.vue
+++ pages/_work/index.vue
@@ -729,7 +729,7 @@
         top: 0;
         left: 0;
         right: 0;
-        bottom: 30px;
+        bottom: 24px;
         z-index: 1;
         background: rgba(0, 0, 0, 0.4);
         opacity: 0;
