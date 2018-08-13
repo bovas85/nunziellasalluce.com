8f324aea85e55ca684fa6fb5ac061ba9b8766c61
Alessandro Giordo <16958684+bovas85@users.noreply.github.com>
2018-08-13 19:43:04 +0100

    fix for route guard

--- middleware/routeGuard.js
+++ middleware/routeGuard.js
@@ -5,14 +5,16 @@ export default function ({ redirect, params, store }) {
   if (index == null) {
     redirect('/')
   }
-  if (
-    !store.state.projects[index].acf.hero ||
-    !store.state.projects[index].acf.product ||
-    !store.state.projects[index].acf.brand ||
-    !store.state.projects[index].acf.category ||
-    !store.state.projects[index].acf.challenge ||
-    !store.state.projects[index].acf.intro
-  ) {
-    redirect('/')
+  if (store.state.projects[index].acf != null) {
+    if (
+      !store.state.projects[index].acf.hero ||
+      !store.state.projects[index].acf.product ||
+      !store.state.projects[index].acf.brand ||
+      !store.state.projects[index].acf.category ||
+      !store.state.projects[index].acf.challenge ||
+      !store.state.projects[index].acf.intro
+    ) {
+      redirect('/')
+    }
   }
 }
