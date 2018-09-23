62d4dcb9a48310fb0fe0b3e3fd21e546f68c080a
Alessandro Giordo <16958684+bovas85@users.noreply.github.com>
2018-09-23 10:18:53 +0100

    work modularised

---
+++ components/Sections/Work/TheBrand.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="the-brand step" v-if="project.brand != null">
+    <div class="container">
+      <h1 :class="{'animated': animateBrand}">{{project.brand.title}}</h1>
+    </div>
+
+    <div class="timeline" :class="{'animated': animateBrand}">
+      <the-timeline :data="project.brand.items"></the-timeline>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'TheBrand',
+    props: ['project', 'animateBrand'],
+    components: {
+      TheTimeline: () => import('@/components/Sliders/TheTimeline')
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .the-brand {
+    overflow: hidden;
+    position: relative;
+
+    h1 {
+      @include fadeInUp;
+      padding-bottom: 60px;
+      margin-bottom: $gap * 2;
+      max-width: 150px;
+
+      @include media(sm) {
+        margin-bottom: $gap * 3;
+      }
+    }
+
+    .timeline {
+      @include fadeInUp;
+      transition-delay: 0.2s;
+    }
+  }
+</style>
