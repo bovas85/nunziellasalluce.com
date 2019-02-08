1daaf8edfe1f49ddf9fb28a5b7480b4aff92ffa3
Alessandro Giordo <16958684+bovas85@users.noreply.github.com>
2019-02-08 07:32:55 +0000

    Updating workbox options

--- nuxt.config.js
+++ nuxt.config.js
@@ -292,11 +292,11 @@ module.exports = {
     runtimeCaching: [
       {
         urlPattern: 'https://api.nunziellasalluce.com/wp-content/uploads/.*',
-        handler: 'cacheFirst',
+        handler: 'staleWhileRevalidate',
         strategyOptions: {
           cacheName: 'images',
           cacheExpiration: {
-            maxEntries: 10,
+            maxEntries: 30,
             maxAgeSeconds: 300
           },
           cacheableResponse: { statuses: [0, 200] }
@@ -316,13 +316,13 @@ module.exports = {
       },
       {
         urlPattern: 'https://fonts.googleapis.com/.*',
-        handler: 'cacheFirst',
+        handler: 'staleWhileRevalidate',
         method: 'GET',
         strategyOptions: {
           cacheName: 'fonts',
           cacheExpiration: {
             maxEntries: 10,
-            maxAgeSeconds: 300
+            maxAgeSeconds: 30000
           },
           cacheableResponse: { statuses: [0, 200] }
         }
