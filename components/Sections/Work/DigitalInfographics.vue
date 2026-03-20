60f0e0696426149a49aefdc7bca4aba275bd3818
Alessandro Giordo <bovas85@gmail.com>
2026-03-20 22:39:24 +0000

    Reorganize LazyImage components in DigitalInfographics

--- components/Sections/Work/DigitalInfographics.vue
+++ components/Sections/Work/DigitalInfographics.vue
@@ -2,22 +2,7 @@
   <section class="digital-infographics step" v-if="project != null && project.infographics != null">
     <div class="container is-flex">
       <h1 class="title step">{{infographics.title}}</h1>
-      <LazyImage
-        v-if="infographics.image_top != null && infographics.image_top.url"
-        class="image image--top"
-        :class="{'animated': animateDigital}"
-        :hover="false"
-        :image="infographics.image_top"
-        :imageMobile="infographics.image_top"
-      />
-      <LazyImage
-        v-if="infographics.image_bottom != null && infographics.image_bottom.url"
-        class="image image--bottom"
-        :class="{'animated': animateDigital}"
-        :hover="false"
-        :image="infographics.image_bottom"
-        :imageMobile="infographics.image_bottom"
-      />
+      
       <LazyImage
           v-if="infographics.video_top != null"
           class="image image--top2"
@@ -33,6 +18,23 @@
           :hover="false"
           :videoMobile="infographics.video_bottom"
           :videoDesktop="infographics.video_bottom"
+      />
+
+      <LazyImage
+        v-if="infographics.image_top != null && infographics.image_top.url"
+        class="image image--top"
+        :class="{'animated': animateDigital}"
+        :hover="false"
+        :image="infographics.image_top"
+        :imageMobile="infographics.image_top"
+      />
+      <LazyImage
+        v-if="infographics.image_bottom != null && infographics.image_bottom.url"
+        class="image image--bottom"
+        :class="{'animated': animateDigital}"
+        :hover="false"
+        :image="infographics.image_bottom"
+        :imageMobile="infographics.image_bottom"
       />
     </div>
   </section>
