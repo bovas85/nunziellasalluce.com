2ab85496a5ca1687d3d193311a6ddbab448be8a9
Alessandro Giordo (Alex), Vodafone UK <alessandro.giordo@vodafone.com>
2026-07-08 22:09:11 +0100

    chore: ignore archive/ directory in ESLint and Markdownlint configurations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,11 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-    // Your custom configs here
-)
+export default withNuxt({
+  ignores: [
+    'archive/**',
+    'dist/**',
+    '.nuxt/**',
+    '.output/**'
+  ]
+})
