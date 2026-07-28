8f04801cf3fc86690aa4aad7c307c21b6298e0ac
Alessandro Giordo (Alex), Vodafone UK <alessandro.giordo@vodafone.com>
2026-07-28 08:28:03 +0100

    mc

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -47,24 +47,24 @@ export default defineNuxtConfig({
   },
   routeRules: {
     "/wp-json/wp/v2/pages/18/**": {
-      proxy: "https://nunziella.moustachedesign.xyz/wp-json/wp/v2/pages/18/**",
+      proxy: "https://nunziella.moustachedesign.fyi/wp-json/wp/v2/pages/18/**",
     },
     "/wp-json/wp/v2/pages/383/**": {
-      proxy: "https://nunziella.moustachedesign.xyz/wp-json/wp/v2/pages/383/**",
+      proxy: "https://nunziella.moustachedesign.fyi/wp-json/wp/v2/pages/383/**",
     },
     "/wp-json/wp/v2/pages/235/**": {
-      proxy: "https://nunziella.moustachedesign.xyz/wp-json/wp/v2/pages/235/**",
+      proxy: "https://nunziella.moustachedesign.fyi/wp-json/wp/v2/pages/235/**",
     },
     "/wp-json/wp/v2/pages/310/**": {
-      proxy: "https://nunziella.moustachedesign.xyz/wp-json/wp/v2/pages/310/**",
+      proxy: "https://nunziella.moustachedesign.fyi/wp-json/wp/v2/pages/310/**",
     },
     "/wp-json/wp/v2/casestudies/**": {
       proxy:
-        "https://nunziella.moustachedesign.xyz/wp-json/wp/v2/casestudies/**",
+        "https://nunziella.moustachedesign.fyi/wp-json/wp/v2/casestudies/**",
     },
     "/wp-json/contact-form-7/v1/contact-forms/78/feedback/**": {
       proxy:
-        "https://nunziella.moustachedesign.xyz/wp-json/contact-form-7/v1/contact-forms/78/feedback/**",
+        "https://nunziella.moustachedesign.fyi/wp-json/contact-form-7/v1/contact-forms/78/feedback/**",
     },
   },
   hooks: {
